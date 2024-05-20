bbcc6db9c24b3cbec0f329b0ba611595ae285886
ITITIU21299 <matofottv@gmail.com>
2024-05-20 21:11:36 +0700

    Frame Size
    720x720

--- src/invoicebilling/LoginFrame.java
+++ src/invoicebilling/LoginFrame.java
@@ -27,16 +27,19 @@ public class LoginFrame extends javax.swing.JFrame {
     private void initComponents() {
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        setPreferredSize(new java.awt.Dimension(720, 720));
+        setResizable(false);
+        setSize(new java.awt.Dimension(720, 720));
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 400, Short.MAX_VALUE)
+            .addGap(0, 727, Short.MAX_VALUE)
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 300, Short.MAX_VALUE)
+            .addGap(0, 121, Short.MAX_VALUE)
         );
 
         pack();
