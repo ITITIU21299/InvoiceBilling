3b3dd6af9a1e9e661e506d5640c4f9c4024dea34
raikyyy <100474917+kphon-sg@users.noreply.github.com>
2024-06-08 02:04:03 +0700

    fix

--- src/invoicebilling/main.java
+++ src/invoicebilling/main.java
@@ -17,17 +17,17 @@ public class main {
     /**
      * @param args the command line arguments
      */static String dbUsername = "sa";
-        static String dbPassword = "123456";
+        static String dbPassword = "huy";
 
         //static String jdbcUrl = "jdbc:sqlserver://sql.bsite.net\\MSSQL2016;databaseName=...;user=...;password=...;encrypt=true;trustServerCertificate=true;";
-    public static String jdbcUrl = "jdbc:sqlserver://localhost:1433;databaseName=INVOICEBILLING;user=sa;password=123456";
+    public static String jdbcUrl = "jdbc:sqlserver://localhost:1433;databaseName=INVOICEBILLING;user=sa;password=h=huy";
 
 
     public static void main(String[] args) throws SQLException {            
         Connection conn = null;
             try {
                 Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
-                String url = "jdbc:sqlserver://localhost:1433;databaseName=INVOICEBILLING;user=sa;password=123456";
+                String url = "jdbc:sqlserver://localhost:1433;databaseName=INVOICEBILLING;user=sa;password=huy";
                 conn = DriverManager.getConnection(url);
             } catch (ClassNotFoundException | SQLException e) {
                 e.printStackTrace();
